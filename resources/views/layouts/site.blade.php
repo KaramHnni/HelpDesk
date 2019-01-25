1fe53423f126e25052de485a5006b8297f00f86d
Henni Karam <karam.tenes@gmail.com>
2019-01-25 19:42:07 +0100

    Setup user Login Pgae

---
+++ resources/views/layouts/site.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('page-title')</title>
+    <link rel="stylesheet" href="{{asset('/assets/css/tailwind.css')}}">
+    <link rel="stylesheet" href="{{asset('/assets/css/site.css')}}">
+
+
+    
+
+</head>
+<body>
+    <div class=" h-screen bg-grey-lightest">
+        @yield('main-content')
+    </div>
+</body>
+</html>
