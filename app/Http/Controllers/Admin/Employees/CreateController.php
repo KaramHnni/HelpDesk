377eb44ec79de3471180a344df095fec865927a7
Henni Karam <karam.tenes@gmail.com>
2019-01-25 18:36:37 +0100

    Finishing creation of employees

--- app/Http/Controllers/Admin/Employees/CreateController.php
+++ app/Http/Controllers/Admin/Employees/CreateController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin\Employees;
 
 use App\Models\Departement;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Employé;
 use App\Http\Controllers\Controller;
 
 class CreateController extends Controller
@@ -20,7 +22,28 @@ class CreateController extends Controller
 
     public function create(Request $request){
 
+        $user = new User;
+        $user->name = $request->nom;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->created_at = now();
+        $user->updated_at = now();
+        $user->save();
+        $employé = new Employé;
+        $employé->user_id = $user->id;
+        $employé->nom = $request->nom;
+        $employé->prenom = $request->prenom;
+        $employé->grade = $request->grade;
+        $employé->departement_id = $request->departement;
+        $employé->bureau = $request->bureau;
+        $employé->created_at = now();
+        $employé->updated_at = now();
+        $employé->status = 1;
+        $employé->save();
+        return redirect()->back()->with(['created' => true]);
         
+
+
 
     }
 }
