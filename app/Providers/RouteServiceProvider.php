9933a179c9dacbc65891b0bf79a9847cb245d418
Henni Karam <karam.tenes@gmail.com>
2019-01-25 10:33:53 +0100

    Add admin guard and login controller

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,7 +39,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
-        $this->mapAdmnRoutes();
+        $this->mapAdminRoutes();
 
         //
     }
@@ -51,6 +51,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
+
     protected function mapWebRoutes()
     {
         Route::middleware('web')
@@ -58,11 +59,20 @@ class RouteServiceProvider extends ServiceProvider
              ->group(base_path('routes/web.php'));
     }
 
+    /**
+     * Define the "admin" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+
     protected function mapAdminRoutes(){
+
         Route::middleware('admin')
-        ->prefix('admin')
-        ->namespace($this->namespace . '\Admin')
-        ->group(base_path('routes/admin.php'));
+             ->prefix('admin')
+             ->namespace($this->namespace .'\Admin')
+             ->group(base_path('routes/admin.php'));
     }
 
     /**
