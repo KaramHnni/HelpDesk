014c7fd3cfda7c3f2ffdbd8cbeac27f04676a304
Henni Karam <karam.tenes@gmail.com>
2019-01-25 12:39:23 +0100

    Setup input names and login route

--- resources/views/pages/admin/auth/connexion.blade.php
+++ resources/views/pages/admin/auth/connexion.blade.php
@@ -8,10 +8,10 @@ Connexion-Administrateur
     <section class="h-screen py-16">
         <div style=""class="bg-white  w-2/5 mx-auto  py-8">
         <h1 style=""class="text-center text-blue-darker font-bold text-2xl my-4">Connexion Admin</h1>
-        <form class="w-3/4 mx-auto rounded my-12 py-4 ">
-            <input style="#48afeb41;" type="text" class="block mx-auto  w-full border border-solid  font-semibold py-2 px-4 rounded mb-8" placeholder="Email">
-            <input type="password" class="block mx-auto w-full border border-solid  font-semibold py-2 px-4 rounded mb-8 " placeholder="Mot de passe">
-            <input type="submit" value="connexion" class="block mx-auto rounded text-white font-bold bg-blue-darker py-2 px-4 ">
+        <form class="w-3/4 mx-auto rounded my-12 py-4" action="{{route('admin.login')}}" method="POST">
+            <input style="#48afeb41;" name="email" type="text" class="block mx-auto  w-full border border-solid  font-semibold py-2 pl-4 rounded mb-8" placeholder="Email">
+            <input type="password" name="password"class="block mx-auto w-full border border-solid  font-semibold py-2 pl-4 rounded mb-8 " placeholder="Mot de passe">
+            <input type="submit" value="connexion" class="block mx-auto rounded text-white font-bold bg-blue-darker py-2 px-2 ">
         </form>
         </div>
     </section>
