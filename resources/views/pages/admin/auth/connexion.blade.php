6083d5bf89d23559b5ff9a0025e4b5d1a63fdccb
Henni Karam <karam.tenes@gmail.com>
2019-01-25 12:33:52 +0100

    Style administrator login page

--- resources/views/pages/admin/auth/connexion.blade.php
+++ resources/views/pages/admin/auth/connexion.blade.php
@@ -3,4 +3,22 @@
 Connexion-Administrateur
 @endsection
 @section('main-content')
+<div style="background:#48afeb31;"class=" h-screen">
+    
+    <section class="h-screen py-16">
+        <div style=""class="bg-white  w-2/5 mx-auto  py-8">
+        <h1 style=""class="text-center text-blue-darker font-bold text-2xl my-4">Connexion Admin</h1>
+        <form class="w-3/4 mx-auto rounded my-12 py-4 ">
+            <input style="#48afeb41;" type="text" class="block mx-auto  w-full border border-solid  font-semibold py-2 px-4 rounded mb-8" placeholder="Email">
+            <input type="password" class="block mx-auto w-full border border-solid  font-semibold py-2 px-4 rounded mb-8 " placeholder="Mot de passe">
+            <input type="submit" value="connexion" class="block mx-auto rounded text-white font-bold bg-blue-darker py-2 px-4 ">
+        </form>
+        </div>
+    </section>
+<div>
 @endsection
+<style>
+p{
+
+}
+</style>
