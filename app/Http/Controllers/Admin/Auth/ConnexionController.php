be7cfb8b972cffe934c1589c8f6909583e7acda5
Henni Karam <karam.tenes@gmail.com>
2019-01-25 12:49:13 +0100

    Define admin authentification method

--- app/Http/Controllers/Admin/Auth/ConnexionController.php
+++ app/Http/Controllers/Admin/Auth/ConnexionController.php
@@ -10,4 +10,19 @@ class ConnexionController extends Controller
     public function show(){
         return view('pages.admin.auth.connexion');
     }
+
+    public function show(Request $request){
+
+        $credentials = $request->only('email,password');
+
+        if(Auth::guard('admin')->attempt($credentials)){
+
+            return redirect()->intended('admin/dashboard');
+
+        }else{
+            return redirect()->back()->with(['credentials'=>false]);
+
+        }
+
+    }
 }
