2ee29861e751137f9de315e139ffaac2cd48e358
Henni Karam <karam.tenes@gmail.com>
2019-01-25 19:09:18 +0100

    Fix activating and desactivating routes

--- routes/admin.php
+++ routes/admin.php
@@ -23,7 +23,7 @@ Route::group(['middleware' => 'auth:admin'],function(){
         route::get('/','IndexController@show')->name('admin.employees');
         route::get('/create','CreateController@show')->name('admin.employees.create');
         route::post('/create','CreateController@create');
-        route::get('/suspendre','StatusController@suspendre')->name('admin.employees.suspendre');
-        route::get('/valider','StatusController@valider')->name('admin.employees.valider');
+        route::get('/{id}/suspendre','StatusController@suspendre')->name('admin.employees.suspendre');
+        route::get('/{id}/valider','StatusController@valider')->name('admin.employees.valider');
     });
 });
