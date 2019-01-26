b5b999a3d49b32322ec6693afac2d69e6fe1367f
Henni Karam <karam.tenes@gmail.com>
2019-01-26 07:41:25 +0100

    Add admin delete employees controller

---
+++ app/Http/Controllers/Admin/Employees/DeleteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Employees;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DeleteController extends Controller
+{
+    public function supprimer($id){
+
+        $employé = Employé::findOrFail($id);
+        $employé->delete();   
+        return redirect()->back()->with(['deleted' => true]);
+    }
+}
