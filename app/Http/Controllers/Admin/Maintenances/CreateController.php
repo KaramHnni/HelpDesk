105c5deee2d473b4b552eef34c176ae8b05f2e91
Henni Karam <karam.tenes@gmail.com>
2019-01-26 12:58:05 +0100

    Add maintenance create method

--- app/Http/Controllers/Admin/Maintenances/CreateController.php
+++ app/Http/Controllers/Admin/Maintenances/CreateController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Admin\Maintenances;
 
+use App\Models\User;
+use App\Models\Maintenance;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -11,5 +13,30 @@ class CreateController extends Controller
 
         return view('pages.admin.maintenances.create');
 
+            }
+
+            public function create(Request $request){
+
+                $user = new User;
+                $user->name = $request->nom;
+                $user->email = $request->email;
+                $user->password = bcrypt($request->password);
+                $user->created_at = now();
+                $user->updated_at = now();
+                $user->save();
+                $maintenance = new Maintenance;
+                $maintenance->user_id = $user->id;
+                $maintenance->nom = $request->nom;
+                $maintenance->prenom = $request->prenom;
+                $maintenance->grade = $request->grade;
+                $maintenance->created_at = now();
+                $maintenance->updated_at = now();
+                $maintenance->status = 1;
+                $maintenance->save();
+                return redirect()->back()->with(['created' => true]);
+                
+        
+        
+        
             }
 }
