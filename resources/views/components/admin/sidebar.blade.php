ee16ec69b7d0b545cf88dd5cb34ee3f2e654a0ca
Henni Karam <karam.tenes@gmail.com>
2019-01-26 12:45:22 +0100

    Design admin maintenance index

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -4,7 +4,7 @@
         <li><a href="#" class="block px-12 py-4 text-grey-dark mb-4 ">MENU</a></li>
     <li class="px-10  {{ request()->is('admin/dashboard') ? 'border-l-4 border-blue-dark border-solid bg-blue-lightest text-blue-dark font-bold ' : '' }} l"><a href="{{route('admin.dashboard')}}" class="   block py-4">Tableau de bord</a></li>
         <li class="px-10 {{ request()->is('admin/employees/*') || request()->is('admin/employees') ? 'border-l-4 border-blue-dark border-solid bg-blue-lightest text-blue-dark font-bold ' : '' }}"><a href="{{route('admin.employees')}}" class="   block py-4">Employés</a></li>
-        <li class="px-10"><a href="#" class="block py-4">Maintenances</a></li>
+        <li class="px-10  {{ request()->is('admin/maintenances/*') || request()->is('admin/maintenances') ? 'border-l-4 border-blue-dark border-solid bg-blue-lightest text-blue-dark font-bold ' : '' }}"><a href="{{route('admin.maintenances')}}" class="block py-4">Maintenances</a></li>
         <li class="px-10"><a href="#" class="block py-4">Departements</a></li>
         <li class="px-10"><a href="#" class="block py-4">Tickets</a></li>
         <li class="px-10 "><a href="#" class="block py-4">Taches</a></li>
---
+++ resources/views/pages/admin/maintenances/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.admin')
+@section('page-title')
+Maintenances
+@endsection
+
+@section('main-content')
+    @include('components.admin.navigation')
+    <div class="flex">
+        @include('components.admin.sidebar')
+        <div class="flex-grow h-screen">
+             <div style="width:97%;" class="mx-auto h-screen my-4 px-2 py-16 bg-white">
+                <div class="flex justify-between items-center px-8">
+                <h2 class="">Les Agents de maintenances</h2>
+                <a href="#" class="bg-blue-dark py-2 font-bold rounded text-white px-4">+</a>
+            </div>
+            @if($maintenances->count()> 0 ) 
+            <table class="w-full border-l border-r border-grey-lighter border-solid  mt-8">
+                <tr class="text-left py-4 bg-grey-lighter">
+                    <td class="py-4 px-2  text-grey-darker"></td>
+                    <td class="py-4 px-2 text-grey-darker"></td>
+                    <td class="py-4 px-2 text-grey-darker"></td>
+                    <td class="py-4 px-2 text-grey-darker"></td>
+                    <td class="py-4 px-2 text-grey-darker"></td>
+                    <td class="py-4 px-2  text-grey-darker"></td>
+                    <td></td>
+                    <td></td>
+                
+                </tr>
+                @foreach($maintenaces as $maintenance)
+                <tr>
+                <td style="width:15%;line-height:1.66;" class="py-2 px-2 border-b border-solid border-grey-lighter"></td>
+                <td style="width:10%;" class="py-2 px-2 border-b border-solid border-grey-lighter"></td>
+                    <td style="width:12%;" class="py-2 px-2 border-b border-solid border-grey-lighter"></td>
+                    <td style="width:10%;" class="py-2 px-2 border-b border-solid border-grey-lighter"></td>
+                    <td style="width:5%;" class="py-2 px-2 border-b border-solid border-grey-lighter"></td>
+                    <td style="width:7%" class="py-2  px-2  border-b border-solid border-grey-lighter"><span class="rounded inline-block px-2 py-2 bg-green-light text-white">{{$employé->StatusName}}</span></td>
+                    @if($maintenance->status == 1) 
+                    <td style="width:5%;"class=" py-2  px-2  border-b border-solid border-grey-lighter "><a  class="text-red-dark "href="">suspendre</a></td>
+                    @endif
+                    @if($maintenance->status== 0)
+                    <td style="width:5%;"class=" py-2  px-2  border-b border-solid border-grey-lighter "><a class="text-blue" href="#">valider</a></td>
+                   @endif
+                   <td style="width:5%;"class=" py-2  px-2  border-b border-solid border-grey-lighter "><a class="text-red-dark" href="#">supprimer</a></td>
+
+                </tr>
+                @endforeach
+            </table>
+            @else
+
+            <h3 class="text-center mt-8 text-grey-dark">Aucun Agent de maintenance n'a été trouvé</h3>
+            @endif
+        </div>
+
+    </div>
+@endsection
